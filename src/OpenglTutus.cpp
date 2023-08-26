5eb70c31cadd47166289dd3a2a273988616cc7d1
Alwaleed Ahmed <alwleedamado@gmail.com>
2023-08-26 14:48:17 +0200

    Initial commit

---
+++ src/OpenglTutus.cpp
@@ -0,0 +1,47 @@
+﻿// OpenglTutus.cpp : Defines the entry point for the application.
+//
+
+#include "OpenglTutus.h"
+#include <gl/glew.h>
+#include <glfw/glfw3.h>
+
+using namespace std;
+
+int main()
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    GLenum init_result = glewInit();
+    if(init_result != GLEW_OK) {
+        glfwTerminate();
+        return -2;
+    }
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
